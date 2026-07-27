71adb39c619e409fb94d54ba53d5af7f13ff4088
Akshat2480 <akshatworkstuffs@gmail.com>
2026-07-27 17:44:20 +0530

    feat: implement security middlewares

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
+const morgan = require("morgan");
+const { rateLimit } = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
 const app = express();
 
 const postRouter = require("./Routes/postRoutes");
 const userRouter = require("./Routes/userRoutes");
 const commentRouter = require("./Routes/commentRoutes");
 const globalErrorHandler = require("./controller/errorController.js");
-const morgan = require("morgan");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  // console.log(req.cookies);
-  next();
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  limit: 100,
 });
+app.use(limiter);
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+app.use(cors());
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
